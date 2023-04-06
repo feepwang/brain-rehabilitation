83a952ac63150096d00301e3327a7f361977718a
feepwang <me@feep.wang>
2023-04-06 22:45:23 +0800

    add exchange_sort

---
+++ include/algorithms/exchange_sort.h
@@ -0,0 +1,35 @@
+#ifndef BR_EXCHANGE_SORT_H_
+#define BR_EXCHANGE_SORT_H_
+
+#include <vector>
+#include <functional>
+
+namespace br {
+inline namespace algorithms {
+
+/**
+ * @brief
+ * differ from bubble sort, exchange sort compare the element at start with other one to find the (smallest)
+ * @tparam T
+ * @tparam CompareFunctor
+ * @param vec
+ * @param functor
+ */
+template <typename T, class CompareFunctor = std::less<T>>
+void ExchangeSort(std::vector<T>& vec, CompareFunctor functor = CompareFunctor()) {
+  for (size_t i = 0, end_index = vec.size() - 1; i < end_index; ++i) {
+    //  first loop, place the smallest element to the first
+    for (size_t j = i + 1; j <= end_index; ++j) {
+      //  second loop, from end to start, compare and swap
+      //  swap the element if functor return true
+      if (functor(vec[j], vec[i])) {
+        std::swap(vec[j], vec[i]);
+      }
+    }
+  }
+}
+
+}  // namespace algorithms
+}  // namespace br
+
+#endif
--- test/sort.cpp
+++ test/sort.cpp
@@ -1,4 +1,5 @@
 #include "algorithms/bubble_sort.h"
+#include "algorithms/exchange_sort.h"
 #include "gtest/gtest.h"
 
 TEST(BubbleSort, LESS) {
@@ -10,5 +11,17 @@ TEST(BubbleSort, LESS) {
 TEST(BubbleSort, GREATER) {
   std::vector<int> vec{3, 2, 1, 5, 4};
   br::BubbleSort(vec, std::greater<int>());
+  EXPECT_EQ(vec, std::vector<int>({5, 4, 3, 2, 1}));
+}
+
+TEST(ExchangeSort, LESS) {
+  std::vector<int> vec{3, 2, 1, 5, 4};
+  br::ExchangeSort(vec);
+  EXPECT_EQ(vec, std::vector<int>({1, 2, 3, 4, 5}));
+}
+
+TEST(ExchangeSort, GREATER) {
+  std::vector<int> vec{3, 2, 1, 5, 4};
+  br::ExchangeSort(vec, std::greater<int>());
   EXPECT_EQ(vec, std::vector<int>({5, 4, 3, 2, 1}));
 }
